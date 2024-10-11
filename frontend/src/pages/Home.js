93846fe11c5dd555649917d795abce43b07f64c3
Ajay chinni <ajay@Ajays-MacBook-Pro.local>
2024-10-11 15:36:51 +0530

    added statusBubbles componnents in home page to display some stats

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import StatusBubbles from '../components/StatusBubbles/StatusBubbles';
 
 function Home() {
   return (
     <div className="home">
-      <h1>Welcome to NextGenHR Home Page</h1>
+      <h1>Welcome to NextGenHR</h1>
+      <StatusBubbles />
     </div>
   );
 }
