518b8572bb7e0d261135cae2d562eafd66fd3ccc
Ajay chinni <ajay@Ajays-MacBook-Pro.local>
2024-10-11 13:28:37 +0530

    added basic Nav bar

---
+++ frontend/src/pages/Candidate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Candidate() {
+  return (
+    <div className="candidate">
+      <h1>Candidate Page</h1>
+    </div>
+  );
+}
+
+export default Candidate;
---
+++ frontend/src/pages/ScheduleInterview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ScheduleInterview() {
+  return (
+    <div className="schedule-interview">
+      <h1>Schedule Interview Page</h1>
+    </div>
+  );
+}
+
+export default ScheduleInterview;
