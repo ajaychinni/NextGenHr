7a29ad35b99ee43c232effc5a667df79305f9558
Ajay chinni <ajay@Ajays-MacBook-Pro.local>
2024-11-24 04:51:38 +0530

    added routers to candidate_resumes, previous_employments, main_skills

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routers import candidates, jobs, applications, interviews
+from .routers import candidates, jobs, applications, interviews, candidate_resumes, previous_employments, main_skills
 from .database import engine, Base
 
 app = FastAPI(title="NextGenHR API")
@@ -12,6 +12,9 @@ app.include_router(candidates.router)
 app.include_router(jobs.router)
 app.include_router(applications.router)
 app.include_router(interviews.router)
+app.include_router(candidate_resumes.router)
+app.include_router(previous_employments.router)
+app.include_router(main_skills.router)
 
 @app.get("/")
 def read_root():
